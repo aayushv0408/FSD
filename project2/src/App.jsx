02e4724029901ad05e70482561707385c0996d0d
aayushv0408 <aayush04082004@gmail.com>
2025-11-03 12:34:10 +0530

    counter

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import Change from './components/change'
-
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  const addValue = () => {
+    if (counter < 20) setCounter(counter + 1)
+  }
+
+  const removeValue = () => {
+    if (counter > 0) setCounter(counter - 1)
+  }
+
   return (
-    <div>
-      <Change />
-    </div>
+    <>
+      <h1>College React</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue}>Add Value</button><br />
+      <button onClick={removeValue}>Remove Value</button>
+    </>
   )
 }
 
