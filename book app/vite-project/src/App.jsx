7c4f87752d442435fe285b43f86e3e133e2da8c1
aayushv0408 <aayush04082004@gmail.com>
2025-11-03 12:54:06 +0530

    Book app

---
+++ book app/vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Book from './components/Book'
+function App() {
+  return (
+    <div>
+    <Book />
+    </div>
+  )
+}
+
+export default App
---
+++ book app/vite-project/src/components/Book.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Book() {
+  return (
+    <div id ="book">
+        <img src="https://via.placeholder.com/150" alt="Book Cover" />
+        <h2>Book Title</h2>
+        <p>Author: John Doe</p>
+        <p>Description: This is a sample book description.</p>
+      
+    </div>
+  )
+}
+
+export default Book
